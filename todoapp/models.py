ffbe6165b483284691f673c40605d8c887711ffa
EnapsTer <andreshah1@yandex.ru>
2021-08-12 16:31:32 +0300

    add template inheritance

--- todoapp/models.py
+++ todoapp/models.py
@@ -4,21 +4,24 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=255)
     about = models.TextField(blank=True)
-    date_of_completion = models.DateTimeField(
-        blank=True,
-        null=True
-    )
+    date_of_completion = models.DateTimeField(blank=True,
+                                              null=True)
     completed = models.BooleanField(default=False)
 
     def is_expired(self):
         from django.utils import timezone
-        return self.date_of_completion < timezone.now()
+        if self.date_of_completion:
+            return self.date_of_completion < timezone.now()
+
+    @property
+    def completed_text(self):
+        return 'completed' if self.completed else 'not completed'
 
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['-date_of_completion']
+        ordering = ['date_of_completion']
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
 
